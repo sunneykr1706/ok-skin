15333a7d5b75ce222969456e620b9d9782edaa97
sunneykr1706 <sunneykumar309@gmail.com>
2021-11-24 23:28:57 +0530

    vc

---
+++ index.js
@@ -0,0 +1,45 @@
+
+/*var visibleDiv = 0;
+
+
+  function showDiv()
+  {
+    $(".grid").hide();
+    $(".grid:eq("+ visibleDiv +")").show();
+   console.log(visibleDiv);
+  }
+  showDiv()
+function showNext()
+{
+  if(visibleDiv== $(".grid").length-1)
+  {
+    visibleDiv = 0;
+    console.log(visibleDiv);
+  }
+  else {
+    visibleDiv++;
+    
+  }
+  showDiv();
+}
+
+
+function showPrev()
+{
+  if (visibleDiv == 0)
+  {
+    visibleDiv= $(".grid").length-1;
+  }
+  else {
+    visibleDiv--;
+  }
+  showDiv();
+}*/
+var naame=localStorage.getItem("user");
+var user1=localStorage.getItem("user1");
+var user2=localStorage.getItem("user2");
+var user2=localStorage.getItem("user3");
+console.log(naame);
+console.log(user1);
+console.log(user2);
+console.log(user3);
